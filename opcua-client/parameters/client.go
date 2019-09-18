41386a85dcb7dba9bad4c7f0adee45a333078ce8
nightghost <maximgradan@gmail.com>
2019-09-18 18:50:08 +0300

    Parameters subscription implemented.

---
+++ opcua-client/parameters/client.go
@@ -0,0 +1,109 @@
+package parameters
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gopcua/opcua/ua"
+
+	"github.com/gopcua/opcua"
+)
+
+// OpcuaClient is a class for interaction with OPC UA server.
+// You just need to connect to the server and then subscribe to certain parameters.
+type OpcuaClient struct {
+	endpoint      string
+	connection    *opcua.Client
+	subscription  *opcua.Subscription
+	ctx           context.Context
+	logger        log.Logger
+	interval      time.Duration
+	parameters    map[uint32]string
+	handleCounter uint32
+}
+
+// Connect establishes connection between server client.
+func (client *OpcuaClient) Connect() error {
+	opts := []opcua.Option{
+		opcua.SecurityModeString("None"),
+		opcua.AuthAnonymous(),
+	}
+
+	client.connection = opcua.NewClient(client.endpoint, opts...)
+	err := client.connection.Connect(client.ctx)
+	client.handleConnectionError(err)
+
+	if err != nil {
+		return err
+	}
+
+	client.subscription, err = client.connection.Subscribe(&opcua.SubscriptionParameters{
+		Interval: client.interval,
+	})
+	client.handleConnectionError(err)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CloseConnection closes the connection and stops
+// receiving parameter updates.
+func (client *OpcuaClient) CloseConnection() {
+	client.connection.Close()
+}
+
+// SubscribeToParameter makes the client receive updates
+// of the specified parameter from teh server.
+func (client *OpcuaClient) SubscribeToParameter(parameter string) error {
+	id, err := ua.ParseNodeID("ns=5;s=" + parameter)
+	client.handleSubscriptionError(err)
+
+	if err != nil {
+		return err
+	}
+
+	request := opcua.NewMonitoredItemCreateRequestWithDefaults(id,
+		ua.AttributeIDValue, client.handleCounter)
+	res, err := client.subscription.Monitor(ua.TimestampsToReturnBoth, request)
+	client.handleSubscriptionError(err)
+
+	if err != nil {
+		return err
+	}
+
+	if status := res.Results[0].StatusCode; status != ua.StatusOK {
+		err = fmt.Errorf("Bad response status")
+		client.handleSubscriptionError(err)
+
+		return err
+	}
+
+	client.parameters[client.handleCounter] = parameter
+	client.handleCounter++
+
+	return nil
+}
+
+// Start starts the update receiving loop.
+func (client *OpcuaClient) Start() {
+	go client.subscription.Run(client.ctx)
+
+	// TODO: receiving parameter updates from the server and sending then to the fan-out.
+}
+
+func (client *OpcuaClient) handleConnectionError(err error) {
+	if err != nil {
+		client.logger.Println("Couldn't connect to the OPC UA server:", err)
+	}
+}
+
+func (client *OpcuaClient) handleSubscriptionError(err error) {
+	if err != nil {
+		client.logger.Println("Couldn't subscribe to the parameter:", err)
+	}
+}
---
+++ opcua-client/parameters/parameters.go
@@ -0,0 +1,28 @@
+package parameters
+
+const (
+	// Temperature is a physical quantity expressing hot and cold.
+	Temperature = "Temperature"
+	// Humidity is the concentration of water vapour present in air.
+	Humidity = "Humidity"
+	// Pressure is the force applied perpendicular to the surface
+	// of an object per unit area over which that force is distributed.
+	Pressure = "Pressure"
+	// Density of a substance is its mass per unit volume.
+	Density = "Dendity"
+	// Volume is the quantity of three-dimensional space
+	// enclosed by a closed surface.
+	Volume = "Volume"
+	// Voltage is the difference in electric potential between two points.
+	Voltage = "Voltage"
+	// Resistance of an object is a measure of its opposition
+	// to the flow of electric current.
+	Resistance = "Resistance"
+	// Amperage is the voltage to resistance ratio.
+	Amperage = "Amperage"
+	// Wavelength is the spatial period of a periodic wave—the
+	// distance over which the wave's shape repeats.
+	Wavelength = "Wavelength"
+	// Frequency is the number of occurrences of a repeating event per unit of time.
+	Frequency = "Frequency"
+)
