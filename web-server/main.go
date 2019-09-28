ce81275489b68a2f6016c016e411d8be0ca416d6
nightghost <maximgradan@gmail.com>
2019-09-28 12:54:13 +0300

    Libraries added.

--- web-server/main.go
+++ web-server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
+}
+
+func measures(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+}
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+
+	http.ListenAndServe(":8080", router)
 }
