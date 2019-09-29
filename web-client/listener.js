bce4c80d5747d1081ba6167520cf184e7f17f968
nightghost <maximgradan@gmail.com>
2019-09-29 17:18:07 +0300

    Web client replaced with a simpler one.

---
+++ web-client/listener.js
@@ -0,0 +1,27 @@
+var socket = new WebSocket("ws://127.0.0.1:5000/measures");
+var output = document.getElementById("data-field");
+var writeMessage = function(message) {
+    output.innerHTML += "<p>service: " + message + "</p>";
+}
+
+socket.onopen = function () {
+    writeMessage("Connection established");
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        writeMessage('Connection closed gracefully');
+    } else {
+        writeMessage('Connection aborted');
+    }
+
+    writeMessage('code: ' + event.code + ' reason: ' + event.reason);
+};
+
+socket.onmessage = function(event) {
+    output.innerHTML += "<p>data: " + event.data + "</p>";
+}
+
+socket.onerror = function(error) {
+    writeMessage("error occurred: " + error.message);
+}
