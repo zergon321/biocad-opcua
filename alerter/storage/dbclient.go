2211b5c41041118e90e38e5e6ca54e2883db7bb5
nightghost <maximgradan@gmail.com>
2019-10-01 22:39:49 +0300

    Database client for alerter created.

--- alerter/storage/dbclient.go
+++ alerter/storage/dbclient.go
@@ -3,9 +3,30 @@ package storage
 import (
 	"biocad-opcua/data"
 	"log"
+	"strings"
 
 	influxdb "github.com/influxdata/influxdb1-client/v2"
 )
+
+func alertToPoint(alert data.Alert) (*influxdb.Point, error) {
+	tags := map[string]string{
+		"parameter": strings.ToLower(alert.Parameter),
+	}
+
+	fields := map[string]interface{}{
+		"lower_bound": alert.LowerBound,
+		"upper_bound": alert.UpperBound,
+		"value":       alert.Value,
+	}
+
+	point, err := influxdb.NewPoint("alerts", tags, fields, alert.Timestamp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return point, nil
+}
 
 // DbClient represents a client for the time-series database.
 type DbClient struct {
@@ -13,7 +34,7 @@ type DbClient struct {
 	database       string
 	logger         *log.Logger
 	influxClient   influxdb.Client
-	subscription   chan data.Measure
+	subscription   chan data.Alert
 	pointsInSeries int
 	stop           chan interface{}
 }
@@ -34,8 +55,105 @@ func (dbclient *DbClient) Connect() error {
 	return nil
 }
 
+// Start starts accepting alerts and writing them to the time-series database.
+func (dbclient *DbClient) Start() {
+	go func() {
+		var (
+			counter int
+			series  influxdb.BatchPoints
+			err     error
+		)
+
+		// Initialize the first series of points.
+		series, err = influxdb.NewBatchPoints(influxdb.BatchPointsConfig{
+			Database:  dbclient.database,
+			Precision: "us",
+		})
+		dbclient.handleCreateSeriesError(err)
+
+		if err != nil {
+			return
+		}
+
+		for {
+			select {
+			case alert := <-dbclient.subscription:
+				// If series is full - write it to the database and create a new one.
+				if (counter+1)%dbclient.pointsInSeries == 0 {
+					err = dbclient.influxClient.Write(series)
+					dbclient.handleWriteToDbError(err)
+
+					series, err = influxdb.NewBatchPoints(influxdb.BatchPointsConfig{
+						Database:  dbclient.database,
+						Precision: "us",
+					})
+					dbclient.handleCreateSeriesError(err)
+
+					if err != nil {
+						return
+					}
+				}
+
+				// If the series is not full yet, add a new point to the series.
+				point, err := alertToPoint(alert)
+				dbclient.handleCreatePointError(err)
+
+				if err != nil {
+					continue
+				}
+
+				series.AddPoint(point)
+				counter++
+
+			case <-dbclient.stop:
+				break
+			}
+		}
+	}()
+}
+
+// GetSubscriptionChannel returns the channel to accept alerts and write them to the time-series database.
+func (dbclient *DbClient) GetSubscriptionChannel() chan<- data.Alert {
+	return dbclient.subscription
+}
+
+// CloseConnection closes the database connection.
+func (dbclient *DbClient) CloseConnection() {
+	dbclient.influxClient.Close()
+}
+
+// NewDbClient creates a new client for the time-series database.
+func NewDbClient(address, database string, logger *log.Logger, pointsInSeries int) *DbClient {
+	return &DbClient{
+		address:        address,
+		database:       database,
+		logger:         logger,
+		pointsInSeries: pointsInSeries,
+		subscription:   make(chan data.Alert),
+		stop:           make(chan interface{}),
+	}
+}
+
 func (dbclient *DbClient) handleDbConnectionError(err error) {
 	if err != nil {
 		dbclient.logger.Println("Couldn't connect to the database:", err)
+	}
+}
+
+func (dbclient *DbClient) handleWriteToDbError(err error) {
+	if err != nil {
+		dbclient.logger.Println("Couldn't write data to the database:", err)
+	}
+}
+
+func (dbclient *DbClient) handleCreateSeriesError(err error) {
+	if err != nil {
+		dbclient.logger.Println("Couldn't create a point series:", err)
+	}
+}
+
+func (dbclient *DbClient) handleCreatePointError(err error) {
+	if err != nil {
+		dbclient.logger.Println("Couldn't create a point for the database:", err)
 	}
 }
