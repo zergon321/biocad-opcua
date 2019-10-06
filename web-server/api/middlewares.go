59ed32f4bea00ac94670c8a482979362457921e6
nightghost <maximgradan@gmail.com>
2019-10-06 22:45:24 +0300

    CORS middleware added.

--- web-server/api/middlewares.go
+++ web-server/api/middlewares.go
@@ -12,18 +12,23 @@ func jsonMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func loggingMiddleware(logger *log.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				requestID, ok := r.Context().Value(0).(string)
+			requestID, ok := r.Context().Value(0).(string)
 
-				if !ok {
-					requestID = "unknown"
-				}
+			if !ok {
+				requestID = "unknown"
+			}
 
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
-			}()
+			logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
 
 			next.ServeHTTP(w, r)
 		})
