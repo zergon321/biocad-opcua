e504374d87e3649c624f6bb5e275f5b11be7e768
Юра <yri066.ru@outlook.com>
2019-10-06 17:31:42 +0300

    бесполезкуй кусок кода

--- web-client/client.js
+++ web-client/client.js
@@ -1,11 +1,16 @@
 var socket = new WebSocket("ws://127.0.0.1:8080/api/measures");
 var output = document.getElementById("data-field");
+let params = [];
+let values = [];
+var LowerBound=0, UpperBound=0;
+var selectParameter = 0;
+var dataLength = 50;
 var writeMessage = function(message) {
-    output.innerHTML += "<p>service: " + message + "</p>";
+    //output.innerHTML += "<p>service: " + message + "</p>";
 }
 
 socket.onopen = function () {
-    writeMessage("Connection established");
+    loadParametrs();
 };
 
 socket.onclose = function (event) {
@@ -19,9 +24,146 @@ socket.onclose = function (event) {
 };
 
 socket.onmessage = function(event) {
-    output.innerHTML += "<p>data: " + event.data + "</p>";
+    var myJson = JSON.parse(event.data);
+    var Time = new Date(myJson.Timestamp);
+    Time.setMilliseconds(0);
+    for(t in params)
+    {
+        for (var i in myJson.Parameters) 
+        {
+            if(i==params[t])
+            {
+                values[t][1].push({x: Time,y:myJson.Parameters[i]});
+                if(values[t][1].length>dataLength)
+                {
+                    values[t][1].shift();
+                }
+            }
+        }
+    }
+    Chart()
 }
 
+
 socket.onerror = function(error) {
-    writeMessage("error occurred: " + error.message);
+    alert("error occurred: " + error.message);
+}
+
+function loadParametrs() {
+    var myParams, i;
+    var x = new XMLHttpRequest();
+    x.open("GET", "http://localhost:8080/api/parameters", true);
+    x.onload = function ()
+    {
+        var j=0;
+        myParams = JSON.parse(x.responseText);
+        for (i in myParams) 
+        {
+            document.getElementById("parameters").innerHTML += "<option value="+ j++ +">" + myParams[i] + "</option>";
+            params.push(myParams[i]);
+            values.push([myParams[i],[]]);
+        }
+        loadBound(params[0]);
+    }
+    x.send(null);
+    x.onerror = function()
+    {
+        alert("Не удалось загрузить параметры");
+    }
+}
+
+function loadBound(myParams) {
+    var myBound;
+    var y = new XMLHttpRequest();
+    y.open("GET", "http://localhost:8080/api/"+myParams+"/bounds", true);
+    y.onload = function ()
+    {
+        myBound=  JSON.parse(y.responseText);
+        document.getElementById("UpperBound").value = myBound.UpperBound;
+        document.getElementById("LowerBound").value = myBound.LowerBound;
+        UpperBound = myBound.UpperBound;
+        LowerBound = myBound.LowerBound;
+        Chart();
+    }
+    y.send(null);
+    y.onerror = function()
+    {
+        UpperBound = 0;
+        LowerBound = 0;
+        alert("Не удалось загрузить значения аварийных установок для: " + myParams);
+    }
+}
+
+function selectedParameter() {
+    selectParameter = document.getElementById("parameters").value;
+    loadBound(params[selectParameter]);
+    Chart();
+}
+
+function editBound() {
+    var d = document.getElementById("parameters").value;
+    var a = document.getElementById("UpperBound").value;
+    var b = document.getElementById("LowerBound").value;
+    setBound(params[d],a,b);
+}
+
+function setBound(myParams,a,b) {
+    var z = new XMLHttpRequest();
+    z.open("PATCH", "http://localhost:8080/api/"+myParams+"/bounds", true);
+    z.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    z.send('{"LowerBound": ' + b + ',"UpperBound": ' + a + '}');
+    z.onerror = function()
+    {
+        alert("Не удалось изменить значения аварийных установок для: " + myParams);
+    }
+    z.onreadystatechange = function() { 
+        if (this.status === 200) {
+            loadBound(myParams);
+        }
+    }
+}
+
+function Chart() {
+    var chart = new CanvasJS.Chart("chartContainer", {
+	exportEnabled: true,
+	title :{
+		text: values[selectParameter][0]
+	},
+	axisY: {
+		includeZero: false,
+		stripLines: [{
+			value:UpperBound,
+            label: "",
+            labelFontColor: "#FF0000"//цвет для верхней границы
+		}]
+    },
+    axisY2: {
+        includeZero: false,
+		stripLines: [{
+			value:LowerBound,
+            label: "",
+            labelFontColor: "#FF0000"//цвет для нижней границы
+		}]
+    },
+    axisX:{     
+        valueFormatString: "hh:mm:ss",
+      },
+	data: [{
+        name: "Type 1 Filter",
+        type: "line",
+        color: "#000FFF",//цвет для линий должен быть одинаковым
+		markerSize: 0,
+		dataPoints: values[selectParameter][1] 
+    },
+    {
+        name: "",
+        type: "line",
+        markerSize: 0,
+        color: "#000FFF",//цвет для линий должен быть одинаковым
+        showInLegend: false,
+        axisYType: "secondary",
+		dataPoints: values[selectParameter][1] 
+    }]
+    });
+    chart.render();
 }
